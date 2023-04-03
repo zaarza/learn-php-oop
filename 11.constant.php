c7d671ccc533cef9b1e8b6611766cd390f6165f3
zaarza <zaarza03@gmail.com>
2023-04-04 00:17:04 +0700

    11

---
+++ 11.constant.php
@@ -0,0 +1,18 @@
+<?php 
+    // Define (name, value): Can't use in class
+    // Const: can use in class
+
+    // Get used to it uppercase
+    
+    const NAMA = "Arza";
+    echo NAMA;
+    
+    echo "<br>";
+
+    define('ANGKA', 10);
+    echo ANGKA;
+
+    echo "<br>";
+
+    echo __FILE__;
+?>
